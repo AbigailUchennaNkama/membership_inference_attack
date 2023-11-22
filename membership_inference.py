53b2570fc8d59fcf2bd7b1c14f1032f998619198
Abigail <110779467+AbigailUchennaNkama@users.noreply.github.com>
2023-11-22 01:45:10 +0100

    Update membership_inference.py

--- membership_inference.py
+++ membership_inference.py
@@ -4,7 +4,6 @@ import torch.optim as optim
 from torch.optim import lr_scheduler
 from torchvision import transforms, models
 from PIL import Image
-from IPython import display
 from IPython.display import HTML
 import os
 import shutil
@@ -19,8 +18,8 @@ from model_architecture import BinaryClassifier, load_model
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Function to load a model
-def load_target_model():
-    local_path = "weights_resnet18_cifar10.pth"
+def load_target_model(target_model_path):
+    local_path = target_model_path
     weights_pretrained = torch.load(local_path, map_location=DEVICE)
 
     model = resnet18(weights=None, num_classes=10)
@@ -42,9 +41,9 @@ def load_attack_model():
 
 # Function to predict membership
 
-def predict_membership(target_model_path, model_class, num_classes, input_data_path):
+def predict_membership(target_model_path, input_data_path):
     # Load target model
-    target_model = load_target_model()
+    target_model = load_target_model(target_model_path)
 
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -78,29 +77,3 @@ def predict_membership(target_model_path, model_class, num_classes, input_data_p
         colored_result = f'<font color="red">{result}</font>'
 
     display(HTML(colored_result))
-
-
-def membership_inference():
-    #download and pre-process CIFAR10
-    print("Creating sample dataset...")
-    dataset_url = "https://s3.amazonaws.com/fast-ai-imageclas/cifar10.tgz"
-    download_url(dataset_url, '.')
-
-    with tarfile.open("./cifar10.tgz", "r:gz") as tar:
-        tar.extractall(path="./data")
-    print('---'*45)
-
-    # Example usage with a datapoint from the CIFAR-10 dataset
-    target_model_path = "weights_resnet18_cifar10.pth"
-    example_image_path = "./data/cifar10/test/cat/0004.png"
-    target_model_class = resnet18()
-    target_num_classes = 10
-    # Make the prediction
-    predict_membership(
-        target_model_path,
-        target_model_class,
-        target_num_classes,
-        example_image_path)
-
-if __name__ == "__main__":
-    membership_inference()
